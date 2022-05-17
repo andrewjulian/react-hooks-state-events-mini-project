e0363cbceef1a005195410ef2cf521b2ef9106dc
+ <+>
2022-05-17 09:23:35 -0600

    TC support version

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList(taskData) {
+
+  console.log("taskData as prop", {taskData})
+
+
+  const taskDisplayList = taskData.map((task, index) => {
+    return  
+        <li key={index}>
+          <Task text={task.text} category={task.category}/>
+        </li> 
+  });
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {/* display a list of tasks using Task component */} 
+      
+      {taskDisplayList}
+
     </div>
   );
 }
